018261c97f61f3479fb6ea652d6569efbf261d3a
Matthew Osborn <matthew@osbornm.com>
2016-02-02 13:13:15 -0800

    move to using portable cf so we don't require sudo to build

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,13 +1,10 @@
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-  wget http://go-cli.s3-website-us-east-1.amazonaws.com/releases/latest/cf-cli_amd64.deb -qO temp.deb && sudo dpkg -i temp.deb
-  rm temp.deb
-
   cd ./devDist
 
-  cf api https://api.uswest.appfog.ctl.io
-  cf login --u $APPFOG_USERNAME --p $APPFOG_PASSWORD --o "uiux" --s "dev"
-  cf push "cyclops-dev" -b https://github.com/cloudfoundry/staticfile-buildpack.git
-  cf logout
+  ../.travis/cf api https://api.uswest.appfog.ctl.io
+  ../.travis/cf login --u $APPFOG_USERNAME --p $APPFOG_PASSWORD --o "uiux" --s "dev"
+  ../.travis/cf push "cyclops-dev" -b https://github.com/cloudfoundry/staticfile-buildpack.git
+  ../.travis/cf logout
 else
   echo "we only deploy the master branch"
 fi
