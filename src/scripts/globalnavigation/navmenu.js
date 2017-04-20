d7f7f2c6a2f3a4f7a975a3b639d68283fb9f19f8
Ignacio Martin <ignacio@elasticbox.com>
2017-04-20 15:14:23 +0200

    Integrate Global navigation Menu into the sample pages

--- src/scripts/globalnavigation/navmenu.js
+++ src/scripts/globalnavigation/navmenu.js
@@ -1,16 +1,45 @@
-/* testing function */
+var currentServiceName = '';
+var currentService = '';
+var servicesList = ['CAM', 'MH', 'MS', 'CLC', 'DCC'];
 
-function toggleMenu(){
-    var servicesMenuContainer = document.getElementById('services-menu-container');
-    var servicesMenuFade = document.getElementById('services-menu-fade');
-    var servicesMenuItem = document.getElementById('navbar-services-menu');
-
+toggleMenu = function() {
+    var servicesMenuContainer, servicesMenuFade, servicesMenuItem;
+    servicesMenuContainer = document.getElementById('services-menu-container');
+    servicesMenuFade = document.getElementById('services-menu-fade');
+    servicesMenuItem = document.getElementById('navbar-services-menu');
     servicesMenuContainer.classList.toggle('hidden');
     servicesMenuFade.classList.toggle('hidden');
     servicesMenuItem.classList.toggle('open');
-
     if (!servicesMenuContainer.classList.contains('hidden')) {
-        insertServicesMenuAt('services-menu', 'DCC', ['CAM', 'MH', 'MS', 'CLC','DCC']);
+        insertServicesMenuAt('services-menu', this.currentService, this.servicesList);
         servicesMenuFade.style.height = '100%';
     }
-}
+};
+
+setServicesList = function(displayList) {
+  return this.servicesList = displayList;
+};
+
+setCurrentService = function(service) {
+  var navbarServicesMenuCurrentService;
+  this.currentService = service;
+  this.currentServiceName = (function() {
+    switch (false) {
+      case service !== 'CAM':
+        return 'Cloud Application Manager';
+      case service !== 'MH':
+        return 'Managed Hosting';
+      case service !== 'MS':
+        return 'Managed Security';
+      case service !== 'CLC':
+        return 'Public Cloud';
+      case service !== 'DCC':
+        return 'Private Cloud';
+    }
+  })();
+  navbarServicesMenuCurrentService = document.getElementById('navbar-services-menu__current_service');
+  navbarServicesMenuCurrentService.textContent = this.currentServiceName;
+};
+
+window.setCurrentService = setCurrentService;
+window.setServicesList = setServicesList;
